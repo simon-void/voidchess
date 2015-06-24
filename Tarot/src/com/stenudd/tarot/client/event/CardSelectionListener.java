db05aaf4ab523229afca13aefc92d1e1e7e467cb
Stephan Schröder <simon.void@gmail.com>
2015-06-24 21:10:10 +0200

    moved project into VoidSchach960, thereby removing my side projects from this repository

--- Tarot/src/com/stenudd/tarot/client/event/CardSelectionListener.java
+++
@@ -1,6 +0,0 @@
-package com.stenudd.tarot.client.event;
-
-public interface CardSelectionListener
-{
-  public void cardSelected(CardSelectedEvent event);
-}
--- VoidSchach960/src/main/java/board/LastMoveProvider.java
+++
@@ -1,8 +0,0 @@
-package board;
-
-import helper.ExtendedMove;
-
-public interface LastMoveProvider
-{
-  ExtendedMove getLastMove();
-}
