ffdfa1a61d66b70d899705c72614902fe0de29bc
stephans <simon.void+github@posteo.net>
2021-08-30 14:06:30 +0200

    updated shadow plugin to 7.0.0
    because that's what Gradle 7.x is meant to be used with

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version Deps.kotlinVersion
     application
-    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 application {
