ef0c4091925d227d131a663748631a6b68a97d01
stephans <simon.void+git@posteo.net>
2022-06-11 03:46:05 +0200

    update Kotlin to 1.7.0

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,8 +1,8 @@
 object Deps {
-    const val languageVersion = "1.6"
-    const val kotlinVersion = "1.6.21"
+    const val languageVersion = "1.7"
+    const val kotlinVersion = "1.7.0"
     const val batikTranscoder = "org.apache.xmlgraphics:batik-transcoder:1.14"
-    private const val coroutinesVersion = "1.6.1"
+    private const val coroutinesVersion = "1.6.2"
 
     val coroutineDeps = listOf(
         "org.jetbrains.kotlinx:kotlinx-coroutines-core:${coroutinesVersion}",
@@ -14,7 +14,7 @@ object Deps {
     val testDeps = listOf(
         "org.jetbrains.kotlin:kotlin-test:$kotlinVersion",
         "org.testng:testng:7.5",
-        "io.mockk:mockk:1.12.3",
+        "io.mockk:mockk:1.12.4",
     )
 }
 
