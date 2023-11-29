24d80bf7cac460deecac8586b05ef99226c4e463
stephans <simon.void@gmx.de>
2023-11-30 00:02:39 +0100

    update to Gradlew 8.5 and Kotlin 1.9.21

--- module-common/src/main/kotlin/voidchess/common/helper/RuntimeFacade.kt
+++ module-common/src/main/kotlin/voidchess/common/helper/RuntimeFacade.kt
@@ -1,12 +1,9 @@
 package voidchess.common.helper
 
-import java.text.NumberFormat
-
 object RuntimeFacade {
     private val runtime = Runtime.getRuntime()
 
     fun collectGarbage() {
-        runtime.runFinalization()
         runtime.gc()
         Thread.yield()
     }
